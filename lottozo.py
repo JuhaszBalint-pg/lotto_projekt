0159baffe55e2d28fe4017b2bb2e1b97f2098068
Rónai Márton János <ronai.marton.janos@diak.szbi-pg.hu>
2025-11-26 09:36:40 +0100

    vege

--- lottozo.py
+++ lottozo.py
@@ -7,31 +7,42 @@ Ha tudjátok, többféle lottó játékkal is lehessen játszani!
 '''
 
 import random
+futas = True
+while futas:
+    print('Üdvözlünk a lottó játékunkban')
 
-print('Üdvözlünk a lottó játékunkban')
+    jatekos_tipp = []
+    tippnum = 0
+    print('Add meg a tippjeid!')
+    while tippnum != 5:
+        useri = int(input(''))
+        if 0 < useri < 100:
+            tippnum += 1
+            jatekos_tipp.append(useri)
+        else: 
+            print('0-100 között ajd meg számokat!')
+    print(jatekos_tipp)
 
-jatekos_tipp = []
-tippnum = 0
-print('Add meg a tippjeid!')
-while tippnum != 5:
-    useri = int(input(''))
-    if 0 < useri < 100:
-        tippnum += 1
-        jatekos_tipp.append(useri)
-    else: 
-        print('0-100 között ajd meg számokat!')
-print(jatekos_tipp)
+    gepsors = []
+    for i in range (1,6):
+            gepsors.append(random.randint(1, 90))
 
-gepsors = []
-for i in range (1,6):
-        gepsors.append(random.randint(1, 100))
+    for i in gepsors:
+        if gepsors.count(i) > 1:
+            try:
+                gepsors.pop(i)
 
-for i in gepsors:
-    if gepsors.count(i) > 1:
-        try:
-            gepsors.pop(i)
+            except IndexError:
+                continue
 
-        except IndexError:
-            continue
+    print(gepsors)
 
-print(gepsors)
+    pontszam = 0
+    for i in gepsors:
+        gepsors.count(i)
+        if i in jatekos_tipp:
+            pontszam += 1
+    print(f"ennyi a pontszámod: {pontszam}")
+    x = input("szeretnél még játszani? (i/n) \n")
+    if x == "n":
+        futas = False
